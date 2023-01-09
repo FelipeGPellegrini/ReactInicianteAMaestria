b4c1274f5ca6e078b2ecfc10ac9b01dd0f3823a8
Felipe Gomes Pellegrini <107892258+FelipeGPellegrini@users.noreply.github.com>
2023-01-09 11:43:05 -0300

    Initial commit

---
+++ intro/hello-world/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World React</h1>
+      <h2>Emmet funcionou</h2>
+      <h6>Clique aqui em baixo</h6>
+      <a
+        target="_blank"
+        href="https://github.com/FelipeGPellegrini"
+        rel="noreferrer"
+      >
+        GitHub
+      </a>
+    </div>
+  );
+}
+
+export default App;
