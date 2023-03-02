2c10012eddfebda6e1d572d987e8113f87020b79
Felipe Gomes Pellegrini <107892258+FelipeGPellegrini@users.noreply.github.com>
2023-03-02 09:33:44 -0300

    add context

--- reactRouter/react-router/src/App.js
+++ reactRouter/react-router/src/App.js
@@ -4,14 +4,16 @@ import About from "./pages/About";
 import Product from "./pages/Product";
 import Info from "./pages/Info";
 import NotFound from "./pages/NotFound";
+import Search from "./pages/Search";
 
 // Components
 
 import Navbar from "./components/Navbar";
+import SearchForm from "./components/SearchForm";
 
 // Config React-Router
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
@@ -19,14 +21,19 @@ function App() {
       <h1>React Router</h1>
       <BrowserRouter>
         <Navbar />
+        <SearchForm />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           {/* Rota Dinâmica */}
           <Route path="/products/:id" element={<Product />} />
           <Route path="/products/:id/info" element={<Info />} />
+          {/* Rota Search */}
+          <Route path="/search" element={<Search />} />
           {/*/ No match route */}
           <Route path="*" element={<NotFound />} />
+          {/* Redirect */}
+          <Route path="/company" element={<Navigate to="/about" />} />
         </Routes>
       </BrowserRouter>
     </div>
